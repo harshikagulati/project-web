c21e4f35a2ac890b2a8fc12172a420f7d55f1150
Harshika Gulati <harshikaagulati@gmail.com>
2026-04-20 12:24:06 +0530

    final cors + preflight fix

--- backend/server.js
+++ backend/server.js
@@ -15,15 +15,15 @@ const MONGO_URI =
 
 app.use(express.json());
 
-// app.use(cors({
-//     origin: ["http://localhost:5173", "http://localhost:5174","http://localhost:5000","https://houseofmedia.onrender.com", "https://project-web-frontend-ljhb.onrender.com/"],
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true
-// }));
-
 app.use(cors({
-  origin: "*"
+    origin: ["http://localhost:5173", "http://localhost:5174","http://localhost:5000","https://houseofmedia.onrender.com", "https://project-web-frontend-ljhb.onrender.com/"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    // credentials: true
 }));
+
+app.options("*", cors());
+
 
 mongoose.connect(MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
